0049e3d4b7ab59c039def210f317c2190526de6c
Nayara <nayaralima@poli.ufrj.br>
2022-09-15 21:55:49 -0300

    produto

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ server.use(express.json())
 server.use(router)
 
 server.listen(process.env.PORT, () => {
-    console.log("Server running on port " + process.env.PORT) 
-})
+    console.log("Server running on port " + process.env.PORT);
+});
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import authRouter from "./auth.router.js";
 import productsRouter from "./products.router.js";
-import cartRouter from "./cart.router.js"
-;
+import cartRouter from "./cart.router.js";
+
 const router = Router()
 router.use(authRouter);
 router.use(productsRouter);
