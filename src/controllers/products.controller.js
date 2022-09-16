7cc60fe641ce9adc1afef5c23023322e3c01a385
Nayara <nayaralima@poli.ufrj.br>
2022-09-16 18:08:55 -0300

    index products

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -5,7 +5,6 @@ const listProducts = async (req, res) => {
     try {
         
         const products = await db.collection('products').find().toArray();
-        await db.products.createIndex({ title: 'text' }, { default_language: 'es' });
 
         res.status(200).send(products);
         
@@ -22,6 +21,8 @@ const listProduct = async (req, res) => {
     const { keyword } = req.query;
 
     try {
+
+        await db.products.createIndex({ title: 'text' }, { default_language: 'es' });
 
         const product =  db
             .collection('products')
