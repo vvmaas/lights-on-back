ae96f26ffa05b15d9c2ece4312b4e3f9c2271088
Nayara <nayaralima@poli.ufrj.br>
2022-09-18 16:34:52 -0300

    autenticação produtos

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import {signIn, signUp, logout} from "../controllers/auth.controller.js"
-import checkRegistration from "../middlewares/checkRegistration.middleware.js"
-import checkAvailability from "../middlewares/checkAvailability.middleware.js"
+import {signIn, signUp, logout} from "../controllers/auth.controller.js";
+import checkRegistration from "../middlewares/checkRegistration.middleware.js";
+import checkAvailability from "../middlewares/checkAvailability.middleware.js";
 import checkToken from "../middlewares/checkToken.middleware.js";
+import checkSession from "../middlewares/checkSession.middleware.js";
 
 const authRouter = Router()
 authRouter.post("/sign-in", checkRegistration, signIn)
 authRouter.post("/sign-up", checkAvailability, signUp)
-authRouter.delete("/logout", checkToken, logout)
+authRouter.delete("/logout", checkToken, checkSession,logout)
 
 export default authRouter
--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import * as productsController from '../controllers/products.controller.js';
+import checkToken from "../middlewares/checkToken.middleware.js"
+import checkSession from "../middlewares/checkSession.middleware.js"
 
 const productsRouter = express.Router();
 
-productsRouter.get('/products', productsController.listProducts);
+productsRouter.get('/products', checkToken, checkSession, productsController.listProducts);
 
 export default productsRouter;
